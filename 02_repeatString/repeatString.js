1f7fe612ecbb40b8977942aea2265e06ea62130c
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-11-23 23:01:15 -0800

    Passed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let userWord = '';
 
+    for (let i = 0; i < num; i++) {
+        userWord += string;
+    }
+    return userWord;
 };
 
 // Do not edit below this line
