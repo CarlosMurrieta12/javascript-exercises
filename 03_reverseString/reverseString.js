1af883f4331f854a2732c4cae4360a38669a5bec
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-11-28 23:38:22 -0800

    Completed exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let userWord = word;
+    let wordArr = [];
+    let i = 0;
 
+    while (i <= userWord.length) {
+        wordArr.push(userWord[userWord.length - i]);
+        i++;
+    };
+
+    wordArr.shift();
+    return wordArr.join('');
 };
 
 // Do not edit below this line
