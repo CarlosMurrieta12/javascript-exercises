a1b48efcd733b0acc7594f00bcf9c553011bcec8
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-12-02 16:57:35 -0800

    Completed 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value, secValue, thirdValue, fourthValue) {
+    let i = 0;
+    while (i < arr.length) {
+        if (arr[i] === value || arr[i] === secValue || arr[i] === thirdValue || arr[i] === fourthValue) {
+            arr.splice(i, 1);
+        }
+        else {
+            i++;
+        }
+    }
 
-};
+
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
