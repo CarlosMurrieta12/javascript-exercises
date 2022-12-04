d27d5e0592f4b725e26cec294b7cb31c47005056
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-12-04 00:42:14 -0800

    Completed exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(firstValue, secondValue) {
+	return firstValue + secondValue;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstValue, secondValue) {
+	return firstValue - secondValue;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumOfNums = 0;
+	for (let i = 0; i < arr.length; i++) {
+    sumOfNums += arr[i];
+  }
+  return sumOfNums;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiOfNums = 1;
+	for (let i = 0; i < arr.length; i++) {
+    multiOfNums *= arr[i];
+  }
+  return multiOfNums;
 };
 
-const power = function() {
-	
+const power = function(firstValue, secondValue) {
+	return firstValue ** secondValue;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let factNum = 1;
+  if (value === 0) {
+    return 1;
+  }
+  else {
+  	for (let i = 1; i <= value; i++) {
+      factNum *= i;
+    }
+    return factNum;
+  }
 };
 
 // Do not edit below this line
