3e96281a34b71979e8f6d4c0e1ce98f3a2ee2708
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-12-04 02:43:29 -0800

    Completed exercie 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr = [];
+    let paliString;
+    let paliArr = [];
+    let thirdString;
+    let secString;
 
+
+    if (string.charAt(string.length - 1) === '.' || string.charAt(string.length - 1) === '!') {
+        secString = string.slice(0, string.length - 1);
+        paliArr = secString.split(' ');
+        thirdString = paliArr.join('');
+        paliString = thirdString.toLowerCase();
+    }
+    else {
+        secString = string.slice();
+        paliArr = secString.split(' ');
+        thirdString = paliArr.join('');
+        paliString = thirdString.toLowerCase();
+    }
+
+    for (let i = 0; i < paliString.length; i++) {
+        arr.push(paliString.charAt(i));
+    }
+
+    if (paliString.charAt(1) === arr[arr.length - 2]) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
