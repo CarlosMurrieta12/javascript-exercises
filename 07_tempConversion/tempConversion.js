2b3d51975c3e3f5f1f3b2319bc3b9ca22e7665a4
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-12-04 00:22:08 -0800

    Completed exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegree) {
+  let cDegree = 0;
+  cDegree = (fDegree - 32) * 5/9;
+  let rounded = Math.round(cDegree * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDegree) {
+  let fDegree = 0;
+  fDegree = (cDegree * 9/5 + 32);
+  let rounded = Math.round(fDegree * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
