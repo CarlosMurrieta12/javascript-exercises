74319a941e6a19953726c481bd64f777ff2b8a45
CarlosMurrieta12 <emilimurrieta@icloud.com>
2022-12-03 23:40:07 -0800

    Completed exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+    let sumOfNum = 0;
+    if (firstValue < 0 || secondValue < 0) {
+        return 'ERROR';
+    }
+    else if (!Number.isInteger(firstValue) || !Number.isInteger(secondValue)) {
+        return 'ERROR';
+    }
+    else if (firstValue < secondValue) {
+        for (let i = firstValue; i <= secondValue; i++) {
+        sumOfNum += i;
+        }
+    }
+    else if (secondValue < firstValue) {
+        for (let i = secondValue; i <= firstValue; i++) {
+            sumOfNum += i;
+        }
+    }
+    return sumOfNum;
 };
 
 // Do not edit below this line
